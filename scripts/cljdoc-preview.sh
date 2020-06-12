34a614e3ff3723fcb976606e3afd87a599aca910
filip <fbielejec@gmail.com>
2020-06-12 16:04:48 +0200

    cleanup script [ci skip]

--- scripts/cljdoc-preview.sh
+++ scripts/cljdoc-preview.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#sudo rm -rf /home/$USER/cljdoc-docker
+# sudo rm -rf /home/$USER/cljdoc-docker
 # mkdir -p /home/$USER/cljdoc-docker
 
 # TODO : parse from defproject
@@ -18,18 +18,6 @@ docker run --rm \
        --git /cljs-orbitdb \
        --rev "feature/cljdoc"
 
-# docker run --rm \
-#        -v "$PWD:/cljs-orbitdb" \
-#        -v "$HOME/.m2:/root/.m2" \
-#        -v /home/$USER/cljdoc-docker:/app/data --entrypoint "clojure" \
-#        cljdoc/cljdoc -A:cli ingest \
-#        -p cljs-orbitdb/cljs-orbitdb
-#        -v "$version" \
-#        --jar /root/.m2/repository/repository/cljs-orbitdb/cljs-orbitdb/$version/cljs-orbitdb-$version.jar \
-#        --pom /root/.m2/repository/cljs-orbitdb/cljs-orbitdb/$version/cljs-orbitdb-$version.pom  \
-#        --git /cljs-orbitdb \
-#        --rev "feature/cljdoc"
-
 docker run --rm
        -v $PWD:/cljs-orbitdb \
        -v "$HOME/.m2:/root/.m2" \
@@ -41,5 +29,4 @@ docker run --rm
 echo "---- cljdoc preview: starting server on port 8000"
 
 docker run --rm -p 8000:8000 -v /home/$USER/cljdoc-docker:/app/data cljdoc/cljdoc
-# http://localhost:8000/d/cljs-orbitdb/cljs-orbitdb/0.0.9-SNAPSHOT
-# http://localhost:8000/d/cljs-orbitdb/cljs-orbitdb/0.0.9-SNAPSHOT/doc/readme
+# http://localhost:8000/d/cljs-orbitdb/cljs-orbitdb/CURRENT
